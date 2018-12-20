14847b1ae199df5a35e51f7859d8dd9487f5d14e
300Bowler <brad@bowloregon.com>
2018-12-20 11:24:39 -0800

    First commit

---
+++ practice.py
@@ -0,0 +1,15 @@
+# Using the format method
+
+n1 = input('Enter a noun:')
+v = input('Enter a verb:')
+adj = input('Enter an adjective:')
+n2 = input('Enter a noun:')
+
+r = '''The {} {} the {} {}'''.format(n1, v, adj, n2)
+
+print(r)
+
+
+
+        
+    
