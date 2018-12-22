2a795f581d457a58a1231dca66338651019995d3
300Bowler <brad@bowloregon.com>
2018-12-21 21:03:43 -0800

    Adding string work file

---
+++ workingWithStrings.py
@@ -0,0 +1,15 @@
+# Working with strings challenges
+
+name = 'Camus'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+
+response_one = input('Enter a noun:')
+response_two = input('Enter a place: ')
+
+print('Yesterday I wrote a ' + response_one + '.  I sent it to ' + response_two + '!')
+
