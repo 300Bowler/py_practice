98519041e917506d80dc7f82f9880d2fd4f6c739
300Bowler <brad@bowloregon.com>
2019-01-20 08:10:09 -0800

    Update module2

--- tstp/module2.py
+++ tstp/module2.py
@@ -1,2 +1,19 @@
 
 import module1
+import csv
+
+with open('HOF.txt', 'w') as f:
+    year = input('Enter year of induction - ')
+    name = input('Enter name of incuctee - ')
+    f.write(year)
+    f.write(name)
+
+with open('movies.csv', 'w', newline='') as f:
+    w = csv.writer(f, delimiter=',')
+    w.writerow(['Top Gun', 'Risky Business', 'Minority Report'])
+    w.writerow(['Titanic', 'The Revenant', 'Inception'])
+    w.writerow(['Training Day', 'Man of Fire', 'Flight'])
+
+    
+
+
